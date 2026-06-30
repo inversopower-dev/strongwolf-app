5d7ca0ffbb45c848b64e3505969bd0140df27c8b
inversopower-dev <inverso.power@gmail.com>
2026-06-30 12:58:07 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "strongwolf-pro-v10";
+const CACHE_NAME = "strongwolf-pro-v12";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./cloud-config.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
